8583b537bf13c011ed38ef6873f685410fd09a92
hi0ki <ansarimehdi518@gmail.com>
2025-02-09 11:08:46 +0100

    ex02 ✅

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -4,25 +4,21 @@
 
 int main()
 {
-    // Create instances of each class
     ClapTrap claptrap("ClapTrap");
     ScavTrap scavtrap("ScavTrap");
     FragTrap fragtrap("FragTrap");
 
-    // Test ClapTrap functionalities
     claptrap.attack("target1");
     claptrap.takeDamage(10);
     claptrap.beRepaired(3);
 
     std::cout << "----------------------------------------------------" << std::endl;
-    // Test ScavTrap functionalities
     scavtrap.attack("target2");
     scavtrap.takeDamage(99);
     scavtrap.beRepaired(5);
     scavtrap.guardGate();
 
     std::cout << "----------------------------------------------------" << std::endl;
-    // Test FragTrap functionalities
     fragtrap.attack("target3");
     fragtrap.takeDamage(15);
     fragtrap.beRepaired(10);
