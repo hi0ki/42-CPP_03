8cbdd6a0cb53efb4bfe81f86b6930d6e798d5db2
hi0ki <ansarimehdi518@gmail.com>
2025-02-09 08:56:43 +0100

    ex01 ✅

--- ex01/src/ScavTrap.cpp
+++ ex01/src/ScavTrap.cpp
@@ -4,36 +4,36 @@ ScavTrap::ScavTrap()
 	this->hitPoints = 100;
 	this->energyPoints = 50;
 	this->attackDamage = 20;
-	std::cout << GREY << "ScavTrap default constructor" << RESET << std::endl;
+	std::cout << GREY << "ScavTrap default constructor called" << RESET << std::endl;
 }
 ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
 {
 	this->hitPoints = 100;
 	this->energyPoints = 50;
 	this->attackDamage = 20;
-	std::cout << GREY << "ScavTrap name constructor" << RESET << std::endl;
+	std::cout << GREY << "ScavTrap name constructor called" << RESET << std::endl;
 }
 
 ScavTrap::ScavTrap(const ScavTrap &obj) : ClapTrap(obj)
 {
-	std::cout << GREY <<  "ScavTrap copy constructor" << RESET << std::endl;
+	std::cout << GREY <<  "ScavTrap copy constructor called" << RESET << std::endl;
 	*this = obj;
 }
 
 ScavTrap::~ScavTrap()
 {
-	std::cout << GREY << "ScavTrap destructor" << RESET << std::endl;
+	std::cout << GREY << "ScavTrap " << this->name << " destructor called" << RESET << std::endl;
 }
 
 ScavTrap	&ScavTrap::operator=(const ScavTrap &obj)
 {
-	std::cout << GREY << "ScavTrap assignation operator" << RESET << std::endl;
+	std::cout << GREY << "ScavTrap assignation operator called" << RESET << std::endl;
 	if (this != &obj)
 	{
-        this->name = obj.name;
-        this->hitPoints = obj.hitPoints;
-        this->energyPoints = obj.energyPoints;
-        this->attackDamage = obj.attackDamage;
+		this->name = obj.name;
+		this->hitPoints = obj.hitPoints;
+		this->energyPoints = obj.energyPoints;
+		this->attackDamage = obj.attackDamage;
 	}
 	return (*this);
 }
